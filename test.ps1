c5dd7cd56432fa89a4ad2e5a71b0ab0046e179d0
Andrei Rezvanov <biz00078@gmail.com>
2022-10-13 18:21:00 +0000

    test update

--- test.ps1
+++ test.ps1
@@ -1,1 +1,13 @@
-Get-ChildItem -Path $HOME | Get-Member
+function Get-TwoSum ([System.Array]$numbers, [int]$target) {
+    $n = $numbers.Count
+    foreach ($i in 0..($n-2)) {
+        foreach ($j in ($i+1)..($n-1)) {
+            if ($numbers[$i] + $numbers[$j] -eq $target) {
+                return @($i, $j)
+            }
+        }
+    }
+}
+
+$expected = Get-TwoSum -numbers @(2, 2, 3) -target 4
+$expected
