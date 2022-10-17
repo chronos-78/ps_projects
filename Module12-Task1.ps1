d9886bb75fc8b0cb2ea574bdbd471f403cb6c615
Andrei Rezvanov <biz00078@gmail.com>
2022-10-17 14:54:17 +0300

    module 12 update

--- Module12-Task1.ps1
+++ Module12-Task1.ps1
@@ -2,31 +2,38 @@ function Converter($str) {
     return [System.Management.ManagementDateTimeConverter]::ToDateTime($str).ToString("HH:mm:ss")
 }
 
-$res = @{}
-
 $processes = Get-WmiObject -Class Win32_Process | Select-Object ProcessId, Name,   
         @{Name='Start Time'; Expression={Converter($_.CreationDate)}}, 
         @{Name='Paged Memory Size (kb)'; Expression={$_.PageFileUsage}}   
 
-$processes | ForEach-Object  { $res[$_.ProcessId] = $_}
-
-foreach($key in $res.Keys) {
-    $res[$key].psobject.properties.Remove('ProcessId')
-}
 
 #Write to CSV
 $csvFilePath = $HOME + "\WmiData.csv"
 $processes | Export-Csv -Path $csvFilePath -NoTypeInformation
+$csvData = Get-Content $csvFilePath | ConvertFrom-Csv
+$csvData
+
+#Write to JSON 
+$jsonFilePath = $HOME + "\WmiData.json"
+$processes| ConvertTo-Json | Out-File $jsonFilePath
+$jsonData = Get-Content $jsonFilePath | ConvertFrom-Json 
+$jsonData
 
 #Write to XML
 $xmlFilePath = $HOME + "\WmiData.xml"
 $xmlData = $processes | ConvertTo-XML -NoTypeInformation
 $xmlData.Save($xmlFilePath)
-
-#Write to JSON 
-$jsonFilePath = $HOME + "\WmiData.json"
-$processes| ConvertTo-Json | Out-File $jsonFilePath
+$xmlData2 = [xml]$(Get-Content $xmlFilePath)
+$xmlData2
 
 #Write to YAML
+$res = @{}
+$processes | ForEach-Object  { $res[$_.ProcessId] = $_}
+
+foreach($key in $res.Keys) {
+    $res[$key].psobject.properties.Remove('ProcessId')
+}
 $yamlFilePath = $HOME + "\WmiData.yml"
-$processes| ConvertTo-Yaml | Out-File $yamlFilePath
+$res | ConvertTo-Yaml | Out-File $yamlFilePath
+$res2 = Get-Content $yamlFilePath | ConvertFrom-Yaml 
+$res2
