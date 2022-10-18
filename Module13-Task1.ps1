cf4886b747651f171e56926a86dda419d6516a73
Andrei Rezvanov <biz00078@gmail.com>
2022-10-18 21:15:45 +0300

    some update

--- Module13-Task1.ps1
+++ Module13-Task1.ps1
@@ -1,23 +1,29 @@
 # 1. Write a script to get from selected remote computer the following data:
 
-$remoteHost = "SRV2019"
+[CmdletBinding()]
+    param (
+        [string]$RemoteHost
+    )
 
-# CPU model and max clock speed
-Get-WmiObject -ComputerName $remoteHost Win32_Processor | Select-Object Name, MaxClockSpeed | Format-Table
+    end {
 
-# Amount of RAM (total, free)
-Get-WmiObject -ComputerName $remoteHost Win32_ComputerSystem | Select-Object TotalPhysicalMemory | Format-Table
-Get-WmiObject -ComputerName $remoteHost Win32_OperatingSystem | Select-Object FreePhysicalMemory | Format-Table
+        # CPU model and max clock speed
+        Get-WmiObject -ComputerName $RemoteHost Win32_Processor | Select-Object Name, MaxClockSpeed | Format-Table
 
-# OS disk free space
-Get-WmiObject -ComputerName $remoteHost Win32_OperatingSystem | ForEach-Object {$sysDrive = $_.SystemDrive} | Format-Table
-Get-WmiObject -ComputerName $remoteHost Win32_LogicalDisk | Where-Object {$_.DeviceID -eq $sysDrive} | Select-Object DeviceID, FreeSpace | Format-Table
+        # Amount of RAM (total, free)
+        Get-WmiObject -ComputerName $RemoteHost Win32_ComputerSystem | Select-Object TotalPhysicalMemory | Format-Table
+        Get-WmiObject -ComputerName $RemoteHost Win32_OperatingSystem | Select-Object FreePhysicalMemory | Format-Table
 
-# OS version
-Get-WmiObject -ComputerName $remoteHost Win32_OperatingSystem | Select-Object Version | Format-Table
+        # OS disk free space
+        Get-WmiObject -ComputerName $RemoteHost Win32_OperatingSystem | ForEach-Object {$sysDrive = $_.SystemDrive} | Format-Table
+        Get-WmiObject -ComputerName $RemoteHost Win32_LogicalDisk | Where-Object {$_.DeviceID -eq $sysDrive} | Select-Object DeviceID, FreeSpace | Format-Table
 
-# Installed hot fixes
-Get-HotFix -ComputerName $remoteHost | Format-Table 
+        # OS version
+        Get-WmiObject -ComputerName $RemoteHost Win32_OperatingSystem | Select-Object Version | Format-Table
 
-# List of stopped services
-Get-WmiObject -Class Win32_Service | Where-Object {$_.State -eq "Stopped"} | Format-Table
+        # Installed hot fixes
+        Get-HotFix -ComputerName $RemoteHost | Format-Table 
+
+        # List of stopped services
+        Get-WmiObject -Class Win32_Service | Where-Object {$_.State -eq "Stopped"} | Format-Table
+}
--- Module13-Task2.ps1
+++ Module13-Task2.ps1
@@ -1,42 +1,48 @@
 # 2. Write a script to get all interactive and remote logon sessions for the selected remote computer. 
 # And for every session find associated user information.
 
-function GetLogonID($string) {
-    $null = $string -match '"(?<LogonID>\d+)"$'
-    return $Matches['LogonID']
-}
+[CmdletBinding()]
+    param (
+        [string]$RemoteHost
+    )
 
-function GetUser($string) {
-    $null = $string -match 'Name="(?<User>.+)"$'
-    return $Matches['User']
-}
+    end {
 
-function GetAccountByName($userAccounts, $userName) {
-    return $userAccounts | Where-Object {$userAccounts.Name -eq $userName }
-}
+        function GetLogonID($string) {
+            $null = $string -match '"(?<LogonID>\d+)"$'
+            return $Matches['LogonID']
+        }
 
-$remoteHost = "SRV2019"
+        function GetUser($string) {
+            $null = $string -match 'Name="(?<User>.+)"$'
+            return $Matches['User']
+        }
 
-$loggedUsers = Get-WmiObject -ComputerName $remoteHost Win32_LoggedonUser | Select-Object @{
-    name='LoggedUser'
-    expression={GetUser($_.Antecedent)}
-},
-@{
-    name='LogonID'
-    expression={GetLogonID($_.Dependent)}
-}
+        function GetAccountByName($userAccounts, $userName) {
+            return $userAccounts | Where-Object {$userAccounts.Name -eq $userName }
+        }
 
-$logonSessions = Get-WmiObject -ComputerName $remoteHost Win32_LogonSession | Where-Object {
-    $_.AuthenticationPackage -eq "NTLM" -and 
-    ($_.LogonType -eq "2" -or $_.LogonType -eq "10")
-}  
-$userAccounts = Get-WmiObject -ComputerName $remoteHost Win32_Account
+        $loggedUsers = Get-WmiObject -ComputerName $RemoteHost Win32_LoggedonUser | Select-Object @{
+            name='LoggedUser'
+            expression={GetUser($_.Antecedent)}
+        },
+        @{
+            name='LogonID'
+            expression={GetLogonID($_.Dependent)}
+        }
 
-foreach ($session in $logonSessions) {
-    $session | Format-Table    
-    $users = $loggedUsers | Where-Object {$_.LogonID -eq $session.LogonId}  
-    $users | Format-Table    
-    $userAccount = $userAccounts | Where-Object {$_.Name -eq $users.LoggedUser}
-    $userAccount | Format-Table
-    Write-Host "---------------------------------------------------------------------------------------------------"
+        $logonSessions = Get-WmiObject -ComputerName $RemoteHost Win32_LogonSession | Where-Object {
+            $_.AuthenticationPackage -eq "NTLM" -and 
+            ($_.LogonType -eq "2" -or $_.LogonType -eq "10")
+        }  
+        $userAccounts = Get-WmiObject -ComputerName $RemoteHost Win32_Account
+
+        foreach ($session in $logonSessions) {
+            $session | Format-Table    
+            $loggedUser = $loggedUsers | Where-Object {$_.LogonID -eq $session.LogonId}  
+            $loggedUser | Format-Table    
+            $userAccount = $userAccounts | Where-Object {$_.Name -eq $loggedUser.LoggedUser}
+            $userAccount | Format-Table
+            Write-Host "---------------------------------------------------------------------------------------------------"
+        }
 }
